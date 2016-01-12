54a757b5073e2d1d51473a2d7a8d1cbcbaf18520
Ivan <github-ifermon@sneakemail.com>
2016-01-12 07:41:33 -0500

    Added timestamps the the messages

--- file_org.py
+++ file_org.py
@@ -6,6 +6,7 @@
 """
 from path import path
 from sys import argv
+import time
 
 SEARCH_PATH = '/mnt/usbstorage/security'
 VALID_EXTENSIONS = [".jpg", ".avi"]
@@ -18,12 +19,16 @@ if __name__ == "__main__":
     else:
         search_path = argv[1]
 
+    ts = time.asctime
+
+    print("{}: Starting {}".format(ts(), argv[0]))
+
     src_dir = path(search_path)
 
     for f in src_dir.files():
         # First validate the extension
         if f.ext.lower() not in VALID_EXTENSIONS:
-            print("Invalid extension -- {}".format(f.name))
+            print("{}: Unsupported extension -- {}".format(ts(), f.name))
             continue
 
         # Next get the file name parts (year, month, day)
@@ -36,13 +41,13 @@ if __name__ == "__main__":
         # Stupid hard-coding, but should catch issues in the near term
         # if we have them. I'll worry about it in two years
         if not year.isnumeric():
-            print("Invalid year - {}".format(f))
+            print("{}: Invalid year - {}".format(ts(), f))
             continue
         if not month.isnumeric():
-            print("Invalid year - {}".format(f))
+            print("{}: Invalid year - {}".format(ts(), f))
             continue
         if not day.isnumeric():
-            print("Invalid year - {}".format(f))
+            print("{}: Invalid year - {}".format(ts(), f))
             continue
 
         # Now make directory(s) if necessary
